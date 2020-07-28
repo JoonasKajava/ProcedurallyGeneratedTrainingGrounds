143cb61fa67217982941add0941ae0aa64f6f1cd
Joonas Kajava <me@joonaskajava.com>
2020-07-28 18:12:24 +0300

    restructure

--- Assets/Scripts/Environment/Terrain/GeometryGeneration.cs
+++ Assets/Scripts/Environment/Terrain/GeometryGeneration.cs
@@ -13,18 +13,17 @@ public class GeometryGeneration : MonoBehaviour
 
     [Tooltip("Sets scale of the noise")]
     public float Scale = 2.0f;
-    public float Frequency = 0.02f;
+    public float Frequency = 0.015f;
+    public float IslandSize = 200.0f;
 
+    public Material TerrainMaterial;
 
     private Terrain Terrain;
     private FastNoise NoiseGenerator;
 
     private JobMonitor JobMonitor;
 
-
-    private float[,] HeightMap;
-    private float MaxHeight { get; set; }
-    private float MinHeight { get; set; }
+    System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
 
     #endregion 
 
@@ -39,123 +38,53 @@ public class GeometryGeneration : MonoBehaviour
         Seed = Seed > 0 ? Seed : new System.Random().Next(1, 996954);
 
         JobMonitor = JobMonitor.CreateInstance();
-        JobMonitor.Title = "Terrain Generation";
-        JobMonitor.CreateInstance().Title = "test";
-        JobMonitor.CreateInstance().Title = "jotain";
+        JobMonitor.Title = "Terrain Heightmap";
 
         #endregion
 
-        NoiseGenerator.SetSeed(Seed);
-        NoiseGenerator.SetNoiseType(FastNoise.NoiseType.Perlin);
-        NoiseGenerator.SetFrequency(Frequency);
 
-        HeightMap = new float[Terrain.terrainData.heightmapWidth, Terrain.terrainData.heightmapHeight];
+        GenerateTerrain();
 
-        Color[,] HeightMapForMonitor = new Color[Terrain.terrainData.heightmapWidth, Terrain.terrainData.heightmapHeight];
-
-        for (int x = 0; x < Terrain.terrainData.heightmapWidth; x++)
-        {
-            for (int y = 0; y < Terrain.terrainData.heightmapHeight; y++)
-            {
-                float noise = NoiseGenerator.GetNoise(x, y);
-                MaxHeight = Math.Max(noise, MaxHeight);
-                MinHeight = Math.Min(noise, MinHeight);
-                HeightMap[x, y] = noise;
-            }
-        }
-
-        HeightMap = ApplyCircularMask(HeightMap);
-        HeightMap = NormalizeHeightMap(HeightMap);
-
-        for (int x = 0; x < Terrain.terrainData.heightmapWidth; x++)
-        {
-            for (int y = 0; y < Terrain.terrainData.heightmapHeight; y++)
-            {
-                float noise = HeightMap[x, y];
-                HeightMapForMonitor[x, y] = new Color(noise, noise, noise, 1);
-            }
-        }
-        JobMonitor.ImageArray = HeightMapForMonitor;
-
-
-        Terrain.terrainData.SetHeights(0, 0, HeightMap);
     }
 
+    public void GenerateTerrain()
+    {
+        stopwatch.Start();
+        Heightmap map = new Heightmap()
+        {
+            Seed = Seed,
+            Width = Terrain.terrainData.heightmapResolution,
+            Height = Terrain.terrainData.heightmapResolution,
+            Frequency = Frequency
+        };
+        map.Generate();
+        map.Normalize();
+        map.ApplyCircularMask(IslandSize);
+        stopwatch.Stop();
+        Debug.Log(string.Format("lasted: {0}", stopwatch.ElapsedMilliseconds));
+        stopwatch.Reset();
+        Terrain.terrainData.SetHeights(0, 0, map.Map);
+        JobMonitor.ImageArray = map.GetColored(150, 150);
+        var NewTexture = new Texture2D(map.Map.GetLength(0), map.Map.GetLength(1));
+        NewTexture.SetPixels(map.GetColored(513, 513).ToFlat());
+        NewTexture.Apply();
+        TerrainMaterial.SetTexture("_MainTex", NewTexture);
+    }
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-
-    #region Noise operations
-    private float[,] NormalizeHeightMap(float[,] heightmap)
-    {
-        int Width = heightmap.GetLength(0);
-        int Height = heightmap.GetLength(1);
-
-        var ModifiedHeightMap = new float[Width, Height];
-
-        for (var x = 0; x < Width; x++)
+        if(Input.mouseScrollDelta.y != 0)
         {
-            for (int y = 0; y < Height; y++)
-            {
-                float Noise = heightmap[x, y];
-
-                ModifiedHeightMap[x, y] = (Noise - MinHeight) / (MaxHeight - MinHeight);
-
-            }
+            IslandSize += Input.mouseScrollDelta.y;
+            GenerateTerrain();
         }
 
-        return ModifiedHeightMap;
-    }
-
-
-
-    private float[,] ApplyCircularMask(float[,] heightmap)
-    {
-
-        int Width = heightmap.GetLength(0);
-        int Height = heightmap.GetLength(1);
-
-
-        float NewMaxHeight = 0f;
-        float NewMinHeight = 0f;
-        
-        float OldMinHeight = MinHeight;
-
-        var ModifiedHeightMap = new float[Width, Height];
-
-        var Mask = new float[Width, Height];
-        for (int x = 0; x < Width; x++)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            for (int y = 0; y < Height; y++)
-            {
-                float Noise = heightmap[x, y];
-                float DistanceX = Math.Abs(x - Width * 0.5f);
-                float DistanceY = Math.Abs(y - Width * 0.5f);
-                float Distance = (float)Math.Sqrt(DistanceX * DistanceX + DistanceY * DistanceY);
-
-                float MaxWidth = Width * 0.5f - 100f;
-                float Delta = Distance / MaxWidth;
-                float Gradient = Delta * Delta;
-
-                float GradientResult = (Math.Max(0.0f, 1.0f - Gradient) * 20f + 1.0f);
-                float NewNoise = 
-                    (Noise + Math.Abs(OldMinHeight)) * GradientResult;
-                Mask[x, y] = NewNoise;
-
-
-                NewMaxHeight = Math.Max(NewNoise, NewMaxHeight);
-                NewMinHeight = Math.Min(NewNoise, NewMinHeight);
-            }
+            Seed = new System.Random().Next(1, 99999);
+            GenerateTerrain();
         }
-        MaxHeight = NewMaxHeight;
-        MinHeight = NewMinHeight;
 
-        return Mask;
     }
-
-    #endregion
 
 }
